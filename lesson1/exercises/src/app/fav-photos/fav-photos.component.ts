1e417e9517e604271ed6948b82fbfc07f7ceaa31
Hannah Miller <hnmiller92@gmail.com>
2020-03-26 18:44:59 -0500

    Part 1 Complete

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Best. Photos. Ever.';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://i.pinimg.com/originals/3a/73/53/3a7353b4fa98c4eb47a768c1fff3537b.jpg';
+  image3 = 'https://www.yourpurebredpuppy.com/dogbreeds/photos2-G/german-shepherd-05.jpg';
 
   constructor() { }
 
